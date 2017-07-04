a908e277161ced0cf3f164da60e675a1039c07a4
薛超 <super.xue@croninfo.com>
2017-07-04 19:20:57 +0800

    add

---
+++ AllSmallDemo/AllSmallClass/媒体播放/MovieThumbRequestViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieThumbRequestViewController.h
+//  AllSmallDemo
+//
+//  Created by Super on 2017/7/4.
+//  Copyright © 2017年 Super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieThumbRequestViewController : UIViewController
+
+@end
---
+++ AllSmallDemo/AllSmallClass/媒体播放/MovieThumbViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieThumbViewController.h
+//  AllSmallDemo
+//
+//  Created by Super on 2017/7/4.
+//  Copyright © 2017年 Super. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieThumbViewController : UIViewController
+
+@end
